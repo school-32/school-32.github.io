8638c58eb5cf8176a7cdc3c826b64772cecabc6f
school-32 <91083674+school-32@users.noreply.github.com>
2021-10-02 20:32:36 +0300

    Add files via upload

---
+++ about.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+<title>Гимназия №32</title>
+<meta charset="utf-8">
+<meta name="description" content="Региональная научно-практическая конференция обучающихся «Создание историко-культурного и духовного наследия России» организуется с 2014 года.">
+<meta name="keywords" content="конференция, гимназия №32, нижнекамск, школа, региональная, научно-практическая, историко-культурного, about">
+<meta name="author" content="Гимназия №3">
+<meta name="copyright" content="Гимназия №3">
+<?php include 'lib/module/sys-meta.php';?>
+<?php include 'lib/module/sys-css.php';?>
+<?php include 'lib/module/sys-js.php';?>
+</head>
+<body>
+<?php include 'lib/module/sys-php.php';?>
+<div class="page-con-content">
+	<div class="banner-con-container darkmode-header">
+		<div id="object-particles">
+		</div>
+		<div class="wavebar-con-container">
+			<div class="wavebar-con-wrap">
+				<div class="wavebar-svg-object">
+				</div>
+				<div class="wavebar-svg-object">
+				</div>
+			</div>
+		</div>
+		<div class='banner-con-title fade-up-onstart'>
+			<div class='banner-tx1-title fade-up-onstart pulsate'>
+				<h1>О нас</h1>
+			</div>
+			<div class='banner-tx2-title fade-up-onstart'>
+				<p>
+					 Конференция Гимназия 32
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="page-con-container">
+		<div class="page-in-container">
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+						<h2>Про это</h2>
+					</div>
+					<div class='container-tx2-block darkmode-txt'>
+						<p>
+							 Описание описание описани е описание описание Описание описание описани е описание описание Описание описание описани е описание описание Описание описание описани е описание описание Описание описание описани е описание описание
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php include 'lib/module/ui-main-footer.php';?>
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,186 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+<title>Гимназия №32</title>
+<meta charset="utf-8">
+<meta name="description" content="Региональная научно-практическая конференция обучающихся «Создание историко-культурного и духовного наследия России» организуется с 2014 года.">
+<meta name="keywords" content="конференция, гимназия №32, нижнекамск, школа, региональная, научно-практическая, историко-культурного, home">
+<meta name="author" content="">
+<meta name="copyright" content="Гимназия №32">
+<meta name="google-site-verification" content="cO1o6sx54cvKxhbnYsABWtl4sYFj9uVKV0DxLKZkWv8"/>
+<?php include 'lib/module/sys-meta.php';?>
+<?php include 'lib/module/sys-css.php';?>
+<?php include 'lib/module/sys-js.php';?>
+<script data-cfasync="false" src='/lib/js/carousel.js?v=1'></script>
+</head>
+<body>
+<img src="/img/graphics/landing/dualshock3.png" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/1.jpg" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/1b.jpg" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/2.jpg" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/2b.jpg" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/3.jpg" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/3b.jpg" width="1" height="1" alt="Preload"/>
+<?php include 'lib/module/sys-php.php';?>
+<?php include 'lib/module/ui-main-video.php'?>
+<?php
+	preloadVideo(
+	'1',
+	'RKsLVfST-z8',
+	'1'
+	);
+	preloadVideo(
+	'2',
+	'_aEVRkxR08k',
+	'2'
+	);
+	preloadVideo(
+	'3',
+	'8FTQgFT6Znw',
+	'3'
+	);
+?>
+<div class="page-con-content landing-con-override">
+		<div class="wavebar-con-container-master">
+			<div class="wavebar-con-wrap">
+			  <div class="wavebar-svg-object"></div>
+			  <div class="wavebar-svg-object"></div>
+			</div>
+		</div>
+	<div class='landing-ico-scrolldown'>
+	</div>
+	<div class="landing-con-main">
+		<div id="object-particles">
+		</div>
+		<div class="content-con-backdrop darkmode-header">
+		</div>
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class='landing-con-left content-expand '>
+					<div class="landing-con-container scale-content-txt-1 fade-in-onload">
+						<div class='landing-ico-logo'>
+						</div>
+						<div class='landing-tx1-heading pulsate'>
+							<h1>VII Региональная<br>
+							 научно-практическая конференция</h1>
+						</div>
+						<div class='landing-tx2-heading'>
+							<p>
+								 «Создание историко-культурного и духовного наследия России»
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class='landing-con-right content-remove '>
+					<div class='landing-img-hero' style="background: url(/img/graphics/landing/dualshock3.png) no-repeat center; background-size: contain;">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-contribute darkmode-feature">
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="github-con-container">
+					<div class="github-con-logo">
+						<div class="github-img-logo">
+						</div>
+					</div>
+					<div class="github-con-mantra">
+						<div class="github-wrp-mantra scale-content-txt-1 ">
+							<div class="github-tx1-mantra darkmode-txt">
+								<h2>Цели</h2>
+							</div>
+							<div class="github-tx2-mantra darkmode-txt">
+								<p>
+									 Духовное, патриотическое и нравственное воспитание обучающихся через исследование прошлого и настоящего своей Родины: 
+									 культурно-исторического наследия; формирование активной жизненной позиции, научно-исследовательского мышления.
+								</p>
+							</div>
+							<a href="https://github.com/rpcs3" target="_blank">
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-adsense">
+		<div class='adsense-tx1-description darkmode-txt'>
+			<span>Место для рекламы</span>
+		</div>
+		<div class="adsense-con-container darkmode-slimbar div-css-board-ad">
+			<div class="adsense-con-frame">
+				<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js">
+						</script>
+				<ins class="adsense-con-insert adsbygoogle" data-ad-client="ca-pub-9076246674760451" data-ad-slot="1835527222"></ins>
+				<script>
+						(adsbygoogle = window.adsbygoogle || []).push({});
+				</script>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-patreon darkmode-feature">
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="patreon-con-container">
+					<div class="patreon-con-logo">
+						<div class="patreon-img-logo">
+						</div>
+					</div>
+					<div class="patreon-con-mantra">
+						<div class="patreon-wrp-mantra scale-content-txt-1 ">
+							<div class="patreon-tx1-mantra darkmode-txt">
+								<h2>Задачи</h2>
+							</div>
+							<div class="patreon-tx2-mantra darkmode-txt">
+								<p>
+									 ●Содействие развитию исследовательской и познавательной деятельности обучающихся.
+									 ●Стимулирование интереса обучающихся к науке и научным открытиям,углубление их знаний в определённых областях истории, краеведения, этнографии.
+									 ●Создание условий для овладения обучающимися умениями и навыками умственного труда и нормами культуры речи.
+									 ●Выявление способных и талантливых обучающихся в области интеллектуального творчества, оказание им теоретико-методологической поддержки.
+								</p>
+							</div>
+							<a href="https://www.patreon.com/Nekotekina" target="_blank">
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-discord darkmode-discord">
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="discord-con-container">
+					<div class="discord-con-logo">
+						<div class="discord-img-logo">
+						</div>
+					</div>
+					<div class="discord-con-mantra">
+						<div class="discord-wrp-mantra scale-content-txt-1">
+							<div class="discord-tx1-mantra darkmode-txt">
+								<h2>Направление</h2>
+							</div>
+							<div class="discord-tx2-mantra darkmode-txt">
+								<p>
+									 1. Секции для учащихся
+									 2. Методический семинар для педагогов
+									 3. Интеллектуальная игра «АБВГДейка»
+								</p>
+							</div>
+							<a href="https://discord.me/rpcs3" target="_blank">
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<svg class="discord-con-animate" preserveaspectratio="none" width="1920" height="100%" viewbox="0 0 1920 330">
+		<path class="discord-svg-animate" fill="#fff" fill-opacity="0.1" d="M140.881198,194.260295 C257.600568,129.32862 342.939626,119.84993 418.009939,203.154617 C493.080251,286.459305 545.728689,70.9046172 636.439626,63.9593047 C727.150564,57.0139922 768.99822,139.670242 858.802907,119.431961 C948.607595,99.1936797 1071.91228,-32.9977266 1243.91228,7.75227342 C1415.91228,48.5022734 1404.10369,208.584305 1508.27166,178.709305 C1612.43963,148.834305 1633.73291,79.913472 1711.63588,98.8569055 C1776.28676,114.577866 1819.96778,221.391836 1889.37253,185.808108 C2017.32661,120.206212 2004.01952,336.769569 2004.01952,336.769569 L271.635881,337 L-149.063338,337 C-149.063338,337 -245.850307,175.637635 -58.0633382,228.867188 C33.8652851,254.92501 64.1722713,236.933925 140.881198,194.260295 Z"></path>
+		</svg>
+	</div>
+	<?php include 'lib/module/ui-main-footer.php';?>
+</div>
+</body>
+</html>
